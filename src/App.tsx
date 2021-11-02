b98d2e9953296c3307720d27042c5938b906239b
lliksis <linsebaer@gmx.de>
2021-11-02 21:10:23 +0100

    Added remark markdown renderer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Editor from './editor/Editor';
+import Preview from './preview/Preview';
 
 const App: React.FC = () => {
     const [doc, setDoc] = React.useState('# Hello, World!\n');
@@ -8,8 +9,13 @@ const App: React.FC = () => {
     }, []);
 
     return (
-        <div className=" flex flex-col h-screen">
-            <Editor initialDoc={doc} onChange={handleDocChange} />
+        <div className="w-screen h-screen md:flex">
+            <div className="w-full md:w-1/2 h-1/2 md:h-full overflow-auto">
+                <Editor initialDoc={doc} onChange={handleDocChange} />
+            </div>
+            <div className="w-full md:w-1/2 h-1/2 md:h-full overflow-auto">
+                <Preview doc={doc} />
+            </div>
         </div>
     );
 };
